a734a7fc09cfee47d9a6b4835ea4735897f5ef55
Petar Markovic <markovic.petar@gmail.com>
2024-11-04 02:04:53 +0100

    Removed unnecessary files and folders

--- build/src/memory_allocator.d
+++
@@ -1,8 +0,0 @@
-build/src/memory_allocator.o: src/memory_allocator.cpp \
- src/../h/memory_allocator.hpp src/../h/../lib/hw.h src/../h/__print.hpp
-
-src/../h/memory_allocator.hpp:
-
-src/../h/../lib/hw.h:
-
-src/../h/__print.hpp:
--- build/src/new.d
+++
@@ -1,8 +0,0 @@
-build/src/new.o: src/new.cpp src/../h/syscall_cpp.hpp \
- src/../h/../h/syscall_c.h src/../h/../h/../lib/hw.h
-
-src/../h/syscall_cpp.hpp:
-
-src/../h/../h/syscall_c.h:
-
-src/../h/../h/../lib/hw.h:
